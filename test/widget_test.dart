43251533a858718ac4ffa6175180d83016c5040c
EricChu <zwt70@outlook.com>
2026-05-21 19:50:28 +0800

    Remove all folders except android

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:coursehub/main.dart';
-
-void main() {
-  testWidgets('App starts correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const CourseHubApp());
-    expect(find.text('课程表'), findsOneWidget);
-  });
-}
